7ec632982b1ba2410029e02b4098a3f4f1f3b600
nanoyume <50348194+nanoyume@users.noreply.github.com>
2022-09-26 20:42:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,22 +16,22 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
+
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
-    
+
 # 持ち越し時間の計算
 # 引数：与えるダメージ, 今の残HP
 @bot.command()
 async def 持越(ctx, damage: int, zan: int):
     outstr = '与えるダメージ：' + str(damage) + '万\n'
     outstr += '残りのHP：' + str(zan) + '万\n'
-    
+
     if damage < zan:
         await ctx.send(outstr + '倒せてないわ')
         return
-  
+
     # (余剰ダメージ÷総ダメージ)×90+20
     overdamage = damage - zan
     time = (float(overdamage) / float(damage)) * 90.0 + 20.0
@@ -39,9 +39,9 @@ async def 持越(ctx, damage: int, zan: int):
 
     if 90 < time:
         time = 90
-    
+
     await ctx.send(outstr + '持ち越し時間は[ ' + str(time) + ' ]秒よ')
-    
+
 #TL秒数改変
 @bot.command()
 async def TL(ctx, time: int, tlstr: str):
@@ -55,7 +55,7 @@ async def TL(ctx, time: int, tlstr: str):
         else:
             orgtime.append(str(Minutes) + ':' + str(Seconds))
         counter+=1
-        
+
     convertime = []
     counter = 89
     for num in range(90):
@@ -68,9 +68,9 @@ async def TL(ctx, time: int, tlstr: str):
         else:
             convertime.append(str(Minutes) + ':' + str(Seconds))
         counter-=1
-    
+
     sp = tlstr.split('\n')
-    
+
     outstr = '持ち越し時間でTLを書き換えたわ'
     outstr += '```c++\n'
     for val in sp:
@@ -82,10 +82,9 @@ async def TL(ctx, time: int, tlstr: str):
 
         outstr += val
         outstr += '\n'
-    
+
     outstr += '```'
     await ctx.send(outstr)
-    
+
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
-
